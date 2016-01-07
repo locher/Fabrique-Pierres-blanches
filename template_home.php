ee6e6ce36e5cc5434c8f032a4da6a3c485612bf9
locher <loic@herrloc.fr>
2016-01-07 14:34:57 +0100

    Détails offre sur home

--- template_home.php
+++ template_home.php
@@ -20,39 +20,33 @@
    </div>
     
     <?php include('arabesque.php');?>
+    
+    <?php if(have_rows('un_cote')):?>
+    <!--Côtés-->
+    <div class="cote-wrapper">
+       
+       <?php
+        while(have_rows('un_cote')) : the_row(); 
+        $cote_img = get_sub_field('image_de_fond');
+        ?>
+       
+       <div class="wrapper-cote">
+            <div class="single-cote" style="background-image: url('<?php echo $cote_img['url']; ?>');">
+                <div class="single-cote-text">
+                    <h1><?php the_sub_field('titre');?></h1>
+                    <p><?php the_sub_field('sous-titre');?></p>
+                </div>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+                <div class="cote-wrapper-bt">
+                    <a href="<?php the_sub_field('lien_bouton');?>" class="bt"><?php the_sub_field('texte_bouton');?></a>
+                </div>
+            </div>
+        </div>
+        
+        <?php endwhile;?>
+           
+    </div>
+    <?php endif;?>  
 
-			<h1><?php the_title(); ?></h1>
-
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php the_content(); ?>
-
-			</article>
-			<!-- /article -->
-
-		<?php endwhile; ?>
-
-		<?php else: ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
-		<?php endif; ?>
-
-		</section>
-		<!-- /section -->
-	</main>
 
 <?php get_footer(); ?>
