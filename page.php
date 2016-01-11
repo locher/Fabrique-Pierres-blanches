5a8a29e42ec15c8e304a0875472171305d440435
locher <loic@herrloc.fr>
2016-01-11 12:20:29 +0100

    Page contact

--- page.php
+++ page.php
@@ -1,37 +1,13 @@
+<?php /* Template Name: Contact template */ get_header(); ?>
+
 <?php get_header(); ?>
-
-	<main role="main">
-		<!-- section -->
-		<section>
-
-			<h1><?php the_title(); ?></h1>
-
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php the_content(); ?>
-
-			</article>
-			<!-- /article -->
-
-		<?php endwhile; ?>
-
-		<?php else: ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
-		<?php endif; ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
+    
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+    
+    <div class="content">
+		<?php the_content();?>
+	</div>
+    
+    <?php endwhile; endif;?>
+		
 <?php get_footer(); ?>
